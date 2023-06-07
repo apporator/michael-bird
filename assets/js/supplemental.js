b40e59791bb6c2eabcea195988edf581592bfc2f
bird <mbird3@gmail.com>
2023-06-07 10:43:20 -0700

    add email modal

--- assets/js/supplemental.js
+++ assets/js/supplemental.js
@@ -1,2 +1,25 @@
 const toChange = document.getElementById("linkedin-button");
 toChange.setAttribute("href", "https://www.linkedin.com/in/mibird/");
+
+const emailModalButton = document.getElementById('email-modal-button');
+const modalWrapper = document.getElementById('modal-wrapper');
+// const modalCloseButton = document.getElementById('close-modal-button');
+const body = document.body;
+
+emailModalButton.addEventListener('mousedown', showModal);
+// modalCloseButton.addEventListener('mousedown', hideModal);
+modalWrapper.addEventListener('mousedown', hideModal);
+
+// 
+function showModal() {
+    // console.log("hit!");
+    modalWrapper.style.display = 'flex';
+    body.style.overflow = 'hidden';
+}
+
+function hideModal(e) {
+    if(e.target === modalWrapper) {
+        modalWrapper.style.display = 'none';
+        body.style.overflow = 'unset';
+    }
+}
